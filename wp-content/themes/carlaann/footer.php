26a093e41dbe2da600bc1dda795634d443813cc6
Carla Harrell <info@carlaharrell.com>
2020-07-29 21:04:26 +0530

    Upload Website

---
+++ wp-content/themes/carlaann/footer.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Carla_Harrell
+ * @since Carla Harrell 1.0
+ */
+?>
+	</div><!-- #main .wrapper -->
+	<footer class="footerContainer">	
+	<?php if ( is_active_sidebar( 'sidebar-4' ) ) : ?>
+    <div class="ftr_mdll_container">
+    	<div class="container">
+        	<div class="row">
+            	<?php dynamic_sidebar( 'sidebar-4' ); ?>
+            </div>
+        </div>
+	</div>
+    <?php endif; ?>
+	<div class="ftrBottomContainer">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<?php if(get_theme_mod('ftr_copyright')):?><p class="copyRight text-center"><?php echo get_theme_mod( 'ftr_copyright' ); ?></p><?php endif; ?>
+				</div>
+
+			</div>
+		</div>
+	</div>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
